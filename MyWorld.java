b3250c5d4f3231bc091129af07f3675eef3d8a52
foo bar <86317585+lol64codes@users.noreply.github.com>
2024-09-17 10:14:16 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,15 +1,10 @@
 import mayflower.*;
-
- 
 public class MyWorld extends World {
-
     private Cat cat;
-    private Dog dog;
-    private String[][]tiles;
-    public MyWorld() 
+    private String[][] tiles;
+    public MyWorld()
     {
         setBackground("img/BG/BG.png");
-        
         //cat = new Cat();
         //addObject(cat, 100, 100);
         //dog = new Dog()
@@ -18,33 +13,27 @@ public class MyWorld extends World {
         //addObject(jack, 100, 300);
         //ninja = new Ninja();
         //addObject(ninja, 100, 400);
-        tiles=new String[6][8];
-        buildWorld(); 
+        tiles = new String[7][5];
+        buildWorld();
                                                  
     }
     public void act()
     {
+        
     }
     public void buildWorld()
     {
-        for(int x = 0; x< tiles.length; x++)
+        for (int x = 0; x < tiles.length; x++)
         {
-            for( int y=0;y< tiles[x].length; y++)
-            {
-                tiles[x][y]="";
-            }
+            tiles[x][4]="ground";
         }
-        for(int y=0;y<tiles[4].length;y++)
+        for (int x = 0; x< tiles.length; x++)
         {
-            tiles[4][y]="ground";
-        }
-        for(int x = 0; x< tiles.length; x++)
-        {
-            for( int y=0;y< tiles[x].length; y++)
+            for (int y=0; y< tiles[x].length; y++)
             {
-                if(tiles[x][y]=="ground")
+                if (tiles[x][y] == "ground")
                 {
-                    addObject(new Block(),x*100, y*98);
+                    addObject(new Block(), x * 128, y * 128);
                 }
             }
         }
