5a24fb37346544a7c3ec6e6a23e63c34fdabc3d9
sathvik-l <sathvikl777@gmail.com>
2024-09-17 09:15:54 -0500

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,20 @@
+
+import mayflower.*;
+public class Block extends Actor
+{
+    // instance variables - replace the example below with your own
+
+    
+    public Block()
+    {
+        setImage("img/Tiles/2.png");
+
+    }
+
+    
+    public void act()
+    {
+         
+
+    }
+}
