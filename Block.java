5c5660a48612048cf563a80f418a44950b3afb73
foo bar <86317585+lol64codes@users.noreply.github.com>
2024-09-19 09:26:38 -0500

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,20 +1,12 @@
-
 import mayflower.*;
 public class Block extends Actor
 {
-    // instance variables - replace the example below with your own
-
-    
     public Block()
     {
         setImage("img/Tiles/2.png");
-
     }
-
-    
     public void act()
     {
-         
-
+        
     }
 }
