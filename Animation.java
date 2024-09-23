6a8cfff57f4ec516f93ba7e023ccb4d1717b3cb5
sathvik-l <sathvikl777@gmail.com>
2024-09-23 10:12:10 -0500

    Update Animation.java

--- Animation.java
+++ Animation.java
@@ -1,27 +1,30 @@
 import mayflower.*;
 public class Animation
 {
-    private MayflowerImage[] frames;
+     private MayflowerImage[] frames;
     private int frameRate;
     private int currentFrame;
+    
+    
     public Animation(int frame, String[] array)
     {
-        frames = new MayflowerImage[array.length];
-        frameRate = frame;
-        currentFrame = 0;
-        for (int i = 0; i < array.length; i++)
+        frames=new MayflowerImage[array.length];
+        frameRate=frame;
+        currentFrame=0;
+        for(int i=0;i<array.length;i++)
         {
-            frames[i] = new MayflowerImage(array[i]);
+            frames[i]= new MayflowerImage(array[i]);
         }
     }
+    
     public MayflowerImage getNextFrame()
     {
         currentFrame++;
-        if (currentFrame % frames.length == 0)
+        if(currentFrame% frames.length==0)
         {
-            currentFrame = 0;
+            currentFrame=0;
         }
-        MayflowerImage frame = frames[currentFrame];
+        MayflowerImage frame= frames[currentFrame];
         return frames[currentFrame];
     }
     public int getFrameRate()
@@ -30,17 +33,19 @@ public class Animation
     }
     public void scale(int w, int h)
     {
-        for (int i = 0; i < frames.length; i++)
+        for(int i=0; i<frames.length; i++)
         {
-            frames[i].scale(w, h); 
+            frames[i].scale(w,h); 
         }
 
     }
     public void setTransparency(int percent)
     {
-        for (int i = 0; i < frames.length; i++)
+         for(int i=0; i<frames.length; i++)
         {
             frames[i].setTransparency(percent); 
         }
     }
+
+   
 }
