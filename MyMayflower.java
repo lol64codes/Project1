ed2b8f22745a2c82594475fe04c66169d5b5e8d7
sathvik-l <sathvikl777@gmail.com>
2024-09-23 10:16:04 -0500

    Update MyMayflower.java

--- MyMayflower.java
+++ MyMayflower.java
@@ -1,14 +1,19 @@
 import mayflower.*;
-public class MyMayflower extends Mayflower
+
+public class MyMayflower extends Mayflower 
 {
+
     public MyMayflower()
     {
-        super("Project 1", 800, 600);
+        //Create a window with 800x600 resolution
+        super("Lab 5", 800, 600);
     }
+
     public void init()
     {
+        //Change this to true to run this program in fullscreen mode
         Mayflower.setFullScreen(false);
-        World w =  new MyWorld();
+        World w =  new TitleScreen();
         Mayflower.setWorld(w);
     }
 }
