4d34992d95a4486ad86f0e5d6be141bba263ceec
sathvik-l <sathvikl777@gmail.com>
2024-09-25 10:33:22 -0500

    Update LevelTwo.java

--- LevelTwo.java
+++ LevelTwo.java
@@ -4,18 +4,25 @@ public class LevelTwo extends LevelOne
 {
     private Cat cat;
     private Dog dog;
-    private String[][]tiles;
+    private String[][] tiles;
    
     public LevelTwo()
     {
         setBackground("img/BG/BG.png");
         
-        
         cat = new Cat();
+        dog=new Dog();
         addObject(cat, 100, 496);
-        
-        tiles=new String[7][9];
+        Yarn y = new Yarn();
+        addObject(y, 500, 150);
+        Yarn r = new Yarn();
+        addObject(r, 200, 450);
+        Yarn n = new Yarn();
+        addObject(n, 400, 320);
+       
         buildWorld(); 
+        
+        showText("Score: " + cat.getScore() + " Lives: " + cat.getLives() , 10, 30, Color.BLACK);                                
     }
     public void act()
     {
@@ -23,6 +30,7 @@ public class LevelTwo extends LevelOne
     }
     public void buildWorld()
     {
+         tiles = new String[7][9];
         for(int x = 0; x< tiles.length; x++)
         {
             tiles[x][4]="ground";
