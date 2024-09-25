e40f84e4ed6086a2b337aebb5010f45ca2778212
sathvik-l <sathvikl777@gmail.com>
2024-09-25 10:25:56 -0500

    Update LevelOne.java

--- LevelOne.java
+++ LevelOne.java
@@ -1,42 +1,52 @@
 import mayflower.*;
-public class LevelOne extends World {
+
+ 
+public class LevelOne extends TitleScreen 
+{
+
     private Cat cat;
-    private String[][] tiles;
-    public LevelOne()
+    private Dog dog;
+    private String[][]tiles;
+    public LevelOne() 
     {
         setBackground("img/BG/BG.png");
+        
         cat = new Cat();
         addObject(cat, 100, 496);
-        //dog = new Dog()
-        //addObject(dog, 100, 200);
-        //jack = new Jack();
-        //addObject(jack, 100, 300);
-        //ninja = new Ninja();
-        //addObject(ninja, 100, 400);
-        tiles = new String[7][5];
-        buildWorld();
-        showText("Score: " + cat.getScore() + " Lives: " + cat.getLives() , 10, 30, Color.BLACK);
-                                                 
+        Yarn y = new Yarn();
+        addObject(y, 600, 150);
+        Yarn r = new Yarn();
+        addObject(r, 300, 450);
+        Yarn n = new Yarn();
+        addObject(n, 500, 310);
+        tiles=new String[7][9];
+        buildWorld(); 
+        showText("Score: " + cat.getScore() + " Lives: " + cat.getLives() , 10, 30, Color.BLACK);                                       
     }
     public void act()
     {
-        
+        if(cat.getScore()== 3)
+        {
+                Mayflower.setWorld(new LevelTwo());
+        }
     }
     public void buildWorld()
     {
-        for (int x = 0; x < tiles.length; x++)
+        for(int x = 0; x< tiles.length; x++)
         {
             tiles[x][4]="ground";
+           
         }
-        for (int x = 0; x< tiles.length; x++)
+        for(int x = 0; x< tiles.length; x++)
         {
-            for (int y=0; y< tiles[x].length; y++)
+            for( int y=0;y< tiles[x].length; y++)
             {
-                if (tiles[x][y] == "ground")
+                if(tiles[x][y]=="ground")
                 {
-                    addObject(new Block(), x * 128, y * 128);
+                    addObject(new Block(),x*128, y*128);
                 }
             }
         }
     }
+    
 }
