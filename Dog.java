b1dee6650ee415a6bb8b5a4d024d78239bc1ac31
sathvik-l <sathvikl777@gmail.com>
2024-09-27 10:15:35 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -16,7 +16,53 @@ public class Dog extends AnimatedActor
     }
     public void act()
     {
-        super.act();        
+        int x = getX();
+        int y = getY();
+        int w = getWidth();
+        int h = getHeight();
+        if (Mayflower.isKeyDown(Keyboard.KEY_D) && (x <= 800 - w))
+        {
+            setLocation (x + 2, y);
+        }
+        if (Mayflower.isKeyDown(Keyboard.KEY_A) && (x >= 0))
+        {
+            setLocation (x - 2, y);
+        }
+        if (Mayflower.isKeyDown(Keyboard.KEY_W) && (y > 0))
+        {
+            setLocation(x, y - 2);
+        }
+        else if (y <= 510 - h)
+        {
+            if (Mayflower.isKeyDown(Keyboard.KEY_D) && (x <= 800 - w)) 
+            {
+                setLocation (x + 2, y + 1);
+            }
+            else if (Mayflower.isKeyDown(Keyboard.KEY_A) && (x >= 0)) 
+            {
+                setLocation (x - 2, y + 1);
+            }
+            else 
+            {
+                setLocation (x, y + 1);
+            }
+        }
+        if (isTouching(Cat.class))
+        {
+            Object a = getOneIntersectingObject(Cat.class);
+            Cat c = (Cat) a;
+            World m = getWorld(); 
+            c.decreaseLives( 1 );
+        
+            if(c.getLives()>0)
+            {
+                c.setLocation(400,300);
+            }
+            else
+            {
+                m.removeObject(c);
+            }
+        }
       
     }
 }
