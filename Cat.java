f7094ef76d2916488c9a4a3ee7097c1ef3908d2f
foo bar <86317585+lol64codes@users.noreply.github.com>
2024-09-27 09:39:00 -0500

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -3,18 +3,18 @@ public class Cat extends AnimatedActor
 {
     private int score;
     private int lives;
-    private Animation walk;    
-    public Cat() 
+    private Animation walk;
+    public Cat()
     {
         String[] catsFrame = new String[10];
         for (int x = 0; x < catsFrame.length; x++)
-        {                                                                                               
-            catsFrame[x]= "img/cat/Walk (" + (x + 1) + ").png";
+        {
+            catsFrame[x] = "img/cat/Walk (" + (x + 1) + ").png";
         }
         walk = new Animation(50, catsFrame);
         setAnimation(walk);
         walk.scale(100, 87);
-        walk.setTransparency(50);                                                     
+        walk.setTransparency(50);
     }
     public void act()
     {
@@ -22,21 +22,31 @@ public class Cat extends AnimatedActor
         int y = getY();
         int w = getWidth();
         int h = getHeight();
-        if (Mayflower.isKeyDown(Keyboard.KEY_RIGHT) && (x <= 800 - w)) 
+        if (Mayflower.isKeyDown(Keyboard.KEY_RIGHT) && (x <= 800 - w))
         {
-            setLocation (x + 1, y);
+            setLocation (x + 2, y);
         }
-        if (Mayflower.isKeyDown(Keyboard.KEY_LEFT) && (x >= 0)) 
+        if (Mayflower.isKeyDown(Keyboard.KEY_LEFT) && (x >= 0))
         {
-            setLocation (x - 1, y);
+            setLocation (x - 2, y);
         }
         if (Mayflower.isKeyDown(Keyboard.KEY_UP) && (y > 0))
         {
             setLocation(x, y - 2);
         }
-        if (Mayflower.isKeyDown(Keyboard.KEY_DOWN) && (y <= 510 - h))
+        else if (y <= 510 - h)
         {
-            setLocation(x, y + 2);
+            if (Mayflower.isKeyDown(Keyboard.KEY_RIGHT) && (x <= 800 - w)) 
+            {
+                setLocation (x + 2, y + 1);
+            }
+            else if (Mayflower.isKeyDown(Keyboard.KEY_LEFT) && (x >= 0)) 
+            {
+                setLocation (x - 2, y + 1);
+            }
+            else {
+                setLocation (x, y + 1);
+            }
         }
     }
     public void increaseScore(int amount)
